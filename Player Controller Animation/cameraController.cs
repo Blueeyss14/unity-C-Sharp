b8d08b3811c5382ca3ae4afd3a7b2050a5e02fc3
unknown <delkanoberutu8@gmail.com>
2024-09-19 16:49:04 +0700

    fix bug controller

--- Player Controller Animation/cameraController.cs
+++ Player Controller Animation/cameraController.cs
@@ -2,24 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float mouseSensitivity;
-    private Transform parent;
+    CharacterController characterController;
 
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float runSpeed;
 
-    private void Start()
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float verticalVelocity;
+    [SerializeField] private float gravity;
+
+    private Animator anim;
+
+    void Start()
     {
-        parent = transform.parent;
-        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    void Update() {
-        Rotate();
+    void Update()
+    {
+        PlayerMoveFn();
+        PlayerJumpFn();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) { 
+            Attack();
+        }
     }
 
-    private void Rotate() {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        parent.Rotate(Vector3.up, mouseX);
+    void PlayerMoveFn()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(verticalInput, 0, verticalInput);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (moveDirection != Vector3.zero) {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                Run();
+            }
+            else
+            {
+                Walk();
+            }
+        } else {
+            Idle();
+        }
+
+        Vector3 movement = moveDirection * moveSpeed * Time.deltaTime;
+        movement.y = verticalVelocity * Time.deltaTime;
+        characterController.Move(movement);
     }
+
+    private void Idle() {
+        moveSpeed = 0;
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk() {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f,0.1f, Time.deltaTime);
+
+    }
+
+    private void Run() {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+
+    void PlayerJumpFn()
+    {
+        if (characterController.isGrounded)
+        {
+            verticalVelocity = 0;
+
+            if (Input.GetKey(KeyCode.Space)) {
+            verticalVelocity = jumpForce;
+            }
+        }
+        verticalVelocity += gravity * Time.deltaTime;
+        Vector3 jumpVector = new Vector3(0, verticalVelocity, 0);
+        characterController.Move(jumpVector * Time.deltaTime);
+    }
+
+    void Attack() {
+        anim.SetTrigger("Attack");
+    } 
 }
