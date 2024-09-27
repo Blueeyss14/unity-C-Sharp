94e67655ea425519ff6d571111d8909224cfd8be
unknown <delkanoberutu8@gmail.com>
2024-09-27 15:37:10 +0700

    update controller

--- Player Controller Animation/pController3.cs
+++ Player Controller Animation/pController3.cs
@@ -1,5 +1,5 @@
-using System Collections;
-using System.Collections.Generic;
+using System Collection;
+using System.Collection.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
@@ -8,26 +8,27 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
 
+    //jump---------------------------------
     [SerializeField] private float jumpForce;
-    [SerializeField] private float jumpVelocity;
-    [SerializeField] private float gravity;
-
+    [SerializeField] private float velocity;
+    [SerializeField] private float gravity; //-9.81
 
     void Start() {
         characterController = GetComponent<CharacterController>();
     }
 
     void Update() {
-        MoveFn();
+        Movement();
+        Jump();
     }
 
-    void MoveFn() {
+    void Movement() {
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(0,0, verticalInput);
-        moveDirection = transform.TransformDirection(moveDirection);
+        Vector moveDirection = new Vector3(0, 0, verticalInput);
+        moveDirection = transform.TransfomrDirection(moveDirection);
 
         if (moveDirection != Vector3.zero) {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ) {
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
                 Run();
             } else {
                 Walk();
@@ -35,11 +36,10 @@ public class PlayerController : MonoBehaviour {
         } else {
             Idle();
         }
-
-        Vector3 movement = moveDirection * moveSpeed * Time.deltaTime;
-        movement.y = jumpVelocity * Time.deltaTime;
+        Vector3 movement = new moveDirection * moveSpeed * Time.deltaTime;
+        movement.y = velocity * Time.deltaTime;
         characterController.Move(movement);
-    } 
+    }
 
     private void Idle() {
         moveSpeed = 0;
@@ -53,17 +53,18 @@ public class PlayerController : MonoBehaviour {
         moveSpeed = runSpeed;
     }
 
-    void JumpFn() {
-        if (characterController.isGrounded) {
-            jumpVelocity = 0;
+    void Jump() {
 
-            if (Input.GetKey(KeyCode.Space)) {
-                jumpVelocity = jumpForce;
+        if (characterController.isGrounded) {
+            velocity = 0;
+            
+            if (Input.GetKey(KeyCode.Space))  {
+                velocity = jumpForce;
             }
         }
 
-        jumpVelocity += gravity * Time.deltaTime;
-        Vector3 jump = new Vector3(0, jumpVelocity, 0);
+        velocity += gravity * Time.deltaTime;
+        Vector3 jump = new Vector3(0, velocity, 0);
         characterController.Move(jump * Time.deltaTime);
     }
 
