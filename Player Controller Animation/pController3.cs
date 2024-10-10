5e021866ce9f0f30c9cf27e298d3aace4311a288
unknown <delkanoberutu8@gmail.com>
2024-10-10 23:26:07 +0700

    add more attack animation

--- Player Controller Animation/pController3.cs
+++ Player Controller Animation/pController3.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     CharacterController characterController;
-    [SerializeField] private float moveValue;
+
+    [SerializeField] private float move;
     [SerializeField] private float walk;
     [SerializeField] private float run;
 
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float velocity;
+    [SerializeField] private float jumpValue;
+    [SerializeField] private float jumpVelocity;
     [SerializeField] private float gravity;
 
     Animator anime;
-
 
     void Start()
     {
@@ -24,79 +25,79 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        PlayerMove();
-        PlayerJump();
+        PlayerMoveFn();
+        PlayerJumpFn();
 
-        if (Input.GetKey(KeyCode.Mouse0)) { 
-            PlayerAttack();
-        }
+        PlayerAttcakFn();
     }
 
-    void PlayerMove() {
 
+    void PlayerMoveFn() {
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(0, 0, verticalInput);
-        moveDirection = transform.TransformDirection(moveDirection);
-
-
-        bool inputLShift = Input.GetKey(KeyCode.LeftShift);
-        bool inputRShift = Input.GetKey(KeyCode.RightShift);
+        Vector3 moveDirection = new Vector3(0,0,verticalInput);
 
         if (Vector3.zero != moveDirection)
         {
-            if (inputLShift || inputRShift)
+            bool lShift = Input.GetKey(KeyCode.LeftShift);
+            bool rShift = Input.GetKey(KeyCode.RightShift);
+
+            if (lShift || rShift)
             {
-                Run();
+                PlayerRun();
             }
             else
             {
-                Walk();
+                PlayerWalk();
             }
         }
-        else
-        {
-            Idle();
+        else { 
+            PlayerIdle();
         }
 
-        Vector3 move = moveDirection * moveValue * Time.deltaTime;
-        move.y = velocity * Time.deltaTime;
-        characterController.Move(move);
-        
+        Vector3 playerMove = moveDirection * move * Time.deltaTime;
+        playerMove.y = jumpVelocity * Time.deltaTime;
+        characterController.Move(playerMove);
     }
 
-    private void Idle() { 
-        moveValue = 0;
+    private void PlayerIdle() { 
+        move = 0;
         anime.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
-        
     }
-    private void Walk()
+    private void PlayerWalk()
     {
-        moveValue = walk;
+        move = walk;
         anime.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+
     }
-    private void Run()
+    private void PlayerRun()
     {
-        moveValue = run;
+        move = run;
         anime.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
     }
 
-    private void PlayerJump()
-    {
+    void PlayerJumpFn() {
         if (characterController.isGrounded) { 
-            velocity = 0;
-            bool jumpSpace = Input.GetKey (KeyCode.Space);
+            jumpVelocity = 0;
 
-            if (jumpSpace) {
-                velocity = jumpForce;    
+            if (Input.GetKey(KeyCode.Space)) {
+                jumpVelocity = jumpValue;
             }
         }
 
-        velocity += gravity  * Time.deltaTime;
-        Vector3 jump = new Vector3(0, velocity, 0);
+        jumpVelocity += gravity * Time.deltaTime;
+        Vector3 jump = new Vector3(0,jumpVelocity,0);
         characterController.Move(jump * Time.deltaTime);
-
     }
-    private void PlayerAttack() {
-        anime.SetTrigger("Attack");
+
+    void PlayerAttcakFn() {
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        { 
+            anime.SetTrigger("Attack");
+        }
+        if (Input.GetKey(KeyCode.Q)) {
+            anime.SetTrigger("AttackQ");
+        }
+
     }
 }
