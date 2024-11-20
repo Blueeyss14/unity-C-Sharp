e09c92f52bb7b5f1060760af6f0bd607876d1429
unknown <delkanoberutu8@gmail.com>
2024-11-20 19:06:57 +0700

    update kecil kecilan

--- Player Controller Animation/pController3.cs
+++ Player Controller Animation/pController3.cs
@@ -95,5 +95,10 @@ public class PlayerController : MonoBehaviour
             playerAnime.SetTrigger("Attack");
         }
 
+        if (Input.GetKey(KeyCode.Q))
+        {
+            playerAnime.SetTrigger("AttackQ");
+        }
+
     }
 }
