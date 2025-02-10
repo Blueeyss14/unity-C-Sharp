c62ccc7559621e7c31fda1ccca275dbddabd1d4b
unknown <delkanoberutu8@gmail.com>
2025-02-11 00:28:56 +0700

    new: controller

--- player_controller/Player.cs
+++ player_controller/Player.cs
@@ -2,36 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    [SerializeField] private CharacterController controller;
-
-    [Header("Player Movement")]
-    [SerializeField] private float playerSpeed;
+    CharacterController playerController;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
     [SerializeField] private float calmTime;
     [SerializeField] private float calmVelocity;
+
+
     void Start()
     {
-        
+        playerController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        PlayerMove();
+        PlayerMoveFn();
     }
 
-    void PlayerMove() {
+    private void PlayerMoveFn() {
         float horizontalMove = Input.GetAxis("Horizontal");
         float verticalMove = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3 (horizontalMove, 0f, verticalMove);
+        Vector3 moveDirection = new Vector3(horizontalMove, 0f,verticalMove);
 
         if (moveDirection.magnitude >= 0.1f) {
+
+            if (Vector3.zero != moveDirection)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                {
+                    PlayerRun();
+                }
+                else
+                {
+                    PlayerWalk();
+                }
+            }
+            else {
+                PlayerIdle();
+            }
+
+
             float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref calmVelocity, calmTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            controller.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+            playerController.Move(moveDirection.normalized * moveSpeed   * Time.deltaTime);
         }
+    }
+
+    private void PlayerIdle() { 
+        moveSpeed = 0f;
+    }
+
+    private void PlayerWalk()
+    {
+        moveSpeed = walkSpeed;
+    }
+
+    private void PlayerRun() {
+        moveSpeed = runSpeed;
     }
 }
