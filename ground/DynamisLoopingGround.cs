b4830a49645e9ddf3ce5395fdab18a1eed75f66b
unknown <delkanoberutu8@gmail.com>
2025-02-24 19:55:10 +0700

    feat: remove far plane with -2 dst

--- ground/DynamisLoopingGround.cs
+++ ground/DynamisLoopingGround.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
+using TMPro;
 using System.Collections.Generic;
 
-
-// [ backwardPlane (-1 index) - defaultPlane (0) - forwardPlane (+1 index) ]
-public class LoopingGround : MonoBehaviour
+public class LoopingPlane : MonoBehaviour
 {
-
     public GameObject planePrefab;
     public Transform player;
+    public TextMeshProUGUI indexText;
     private float planeLength;
     private static HashSet<int> spawnedIndexes = new HashSet<int>(); // save index to plane position
+    private static List<GameObject> spawnedPlanes = new List<GameObject>();
 
     void Start()
     {
         planeLength = GetComponent<Renderer>().bounds.size.z;
-
-        // Start from (-1), tengah (0), dan depan (1)
+        spawnedPlanes.Add(gameObject);
         SpawnPlane(-1);
-        SpawnPlane(0);
         SpawnPlane(1);
     }
 
@@ -25,86 +23,55 @@ public class LoopingGround : MonoBehaviour
     {
         int playerIndex = Mathf.RoundToInt(player.position.z / planeLength);
 
-        // next + 2
+        // character grounded in index then show the text
+        if (indexText != null)
+        {
+            indexText.text = "Index: " + playerIndex;
+        }
+
+        // Z Axis
         if (!spawnedIndexes.Contains(playerIndex + 2) && spawnedIndexes.Contains(playerIndex))
         {
             SpawnPlane(playerIndex + 1);
         }
-        // prev - 2
+
+        // -Z Axis
         if (!spawnedIndexes.Contains(playerIndex - 2) && spawnedIndexes.Contains(playerIndex))
         {
             SpawnPlane(playerIndex - 1);
         }
+
+        CleanupPlanes(playerIndex);
     }
 
     void SpawnPlane(int newIndex)
     {
-        //isClone?
         if (spawnedIndexes.Contains(newIndex)) return;
 
         Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, newIndex * planeLength);
         GameObject newPlane = Instantiate(planePrefab, newPosition, Quaternion.identity);
 
-        spawnedIndexes.Add(newIndex); // add index to list
-
-        // add new script to the plane
-        LoopingGround script = newPlane.AddComponent<LoopingPlane>();
-        script.planePrefab = planePrefab;
-        script.player = player;
-    }
-
-
-    /*
-    
-    public GameObject planePrefab;
-    public Transform player;
-    private float planeLength;
-
-    private static HashSet<int> spawnedIndexes = new HashSet<int>(); // save index to plane position
-
-    void Start()
-    {
-        planeLength = GetComponent<Renderer>().bounds.size.z;
-        int index = Mathf.RoundToInt(transform.position.z / planeLength);
-
-        // add index to first list
-        spawnedIndexes.Add(index);
-    }
-
-    void Update()
-    {
-        int playerIndex = Mathf.RoundToInt(player.position.z / planeLength);
-        int currentIndex = Mathf.RoundToInt(transform.position.z / planeLength);
-
-        // next
-        if (!spawnedIndexes.Contains(currentIndex + 1) && playerIndex > currentIndex)
-        {
-            SpawnPlane(currentIndex + 1);
-        }
-
-        // prev
-        if (!spawnedIndexes.Contains(currentIndex - 1) && playerIndex < currentIndex)
-        {
-            SpawnPlane(currentIndex - 1);
-        }
-    }
-
-    void SpawnPlane(int newIndex)
-    {
-        // isClone?
-        if (spawnedIndexes.Contains(newIndex)) return;
-
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, newIndex * planeLength);
-        GameObject newPlane = Instantiate(planePrefab, newPosition, Quaternion.identity);
-
-        // add index to list
         spawnedIndexes.Add(newIndex);
+        spawnedPlanes.Add(newPlane);
 
-        // add new script to the planeee
         LoopingPlane script = newPlane.AddComponent<LoopingPlane>();
         script.planePrefab = planePrefab;
         script.player = player;
+        script.indexText = indexText;
     }
 
-    */
+    void CleanupPlanes(int playerIndex)
+    {
+        spawnedPlanes.RemoveAll(plane =>
+        {
+            int planeIndex = Mathf.RoundToInt(plane.transform.position.z / planeLength);
+            if (planeIndex < playerIndex - 2 || planeIndex > playerIndex + 2)
+            {
+                spawnedIndexes.Remove(planeIndex);
+                Destroy(plane);
+                return true;
+            }
+            return false;
+        });
+    }
 }
