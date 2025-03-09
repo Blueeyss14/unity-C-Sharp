119a1f24462b0eb4492cb03a2ca73e57c56121d1
unknown <delkanoberutu8@gmail.com>
2025-03-10 00:11:56 +0700

    update: combo attack

--- combo attack/ComboAttack.cs
+++ combo attack/ComboAttack.cs
@@ -3,42 +3,68 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     Animator animator;
-    private int comboIndex = 0;
+    private int comboIndex;
+    private bool isClicked;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        comboIndex = 0;
+        isClicked = true;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            comboIndex++;
-            if (comboIndex == 1)
-            {
-                animator.SetTrigger("Attack");
-                animator.SetInteger("ComboIndex", comboIndex);
-            } else if (comboIndex == 2)
-            {
-                animator.SetTrigger("Attack");
-                animator.SetInteger("ComboIndex", comboIndex);
-            }
-            else if (comboIndex == 3)
-            {
-                animator.SetTrigger("Attack");
-                animator.SetInteger("ComboIndex", comboIndex);
-                comboIndex = 0;
-            }
-
-            //if (comboIndex > 3) {
-            //    animator.Play("Attack1");
-            //    animator.Play("Attack2");
-            //    animator.Play("Attack3");
-            //}
-
+            AttackCombo();
         }
 
+    }
 
+    private void AttackCombo() {
+
+        if (isClicked) {
+            comboIndex++;
+        }
+
+        if (comboIndex == 1) {
+            animator.SetInteger("ComboIndex", 1);
+        }
+
+        /*
+        comboIndex++;
+        if (comboIndex == 1)
+        {
+            animator.SetTrigger("Attack");
+            animator.SetInteger("ComboIndex", comboIndex);
+        }
+        else if (comboIndex == 2)
+        {
+            animator.SetTrigger("Attack");
+            animator.SetInteger("ComboIndex", comboIndex);
+        }
+        else if (comboIndex == 3)
+        {
+            animator.SetTrigger("Attack");
+            animator.SetInteger("ComboIndex", comboIndex);
+            comboIndex = 0;
+        }
+        */
+        //if (comboIndex > 3) {
+        //    animator.Play("Attack1");
+        //    animator.Play("Attack2");
+        //    animator.Play("Attack3");
+        //}
+    }
+
+    public void HandleCombo() { 
+        isClicked = false;
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1") && comboIndex == 1) {
+            animator.SetInteger("ComboIndex", 0);
+            isClicked = true;
+            comboIndex = 0;
+        }
     }
 }
