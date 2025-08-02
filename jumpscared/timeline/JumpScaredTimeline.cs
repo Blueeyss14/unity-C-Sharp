ef5a32fff037c9875a28d26291ab96ebc65f3b61
unknown <delkanoberutu8@gmail.com>
2025-08-02 23:07:50 +0700

    delayed camera

--- jumpscared/timeline/JumpScaredTimeline.cs
+++ jumpscared/timeline/JumpScaredTimeline.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.Playables;
 using System.Collections;
-using System.Linq;
 
 public class TimelineScared : MonoBehaviour
 {
@@ -19,11 +18,11 @@ public class TimelineScared : MonoBehaviour
             triggered = true;
             Debug.Log("Playing on " + scaredCamera.name);
 
-            // Switch camera
-            MainCamera(false);
-            CameraScared(true);
-
             timelineScene.Play();
+
+            // Switch camera (delayed)
+            StartCoroutine(DelayedCameraSwitch(0.12f));
+            //from timeline duration (dynamic)
             StartCoroutine(SwitchBackAfterDelay((float)timelineScene.duration));
         }
     }
@@ -41,6 +40,16 @@ public class TimelineScared : MonoBehaviour
         playerObject.GetComponent<Player>().enabled = !isActive;
         playerObject.GetComponent<Animator>().enabled = !isActive;
 
+    }
+
+    //because the audio delay, so I delayed switching camera
+    IEnumerator DelayedCameraSwitch(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Debug.Log("Switch ke CameraScared");
+        MainCamera(false);
+        CameraScared(true);
     }
 
     IEnumerator SwitchBackAfterDelay(float delay)
