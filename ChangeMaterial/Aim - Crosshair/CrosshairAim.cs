81c9c74dcd2d0a78571e3487f885eaf6954be304
unknown <delkanoberutu8@gmail.com>
2026-01-01 15:02:33 +0700

    add crosshair

---
+++ ChangeMaterial/Aim - Crosshair/CrosshairAim.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairAim : MonoBehaviour
+{
+    public Image crosshair;
+    public Camera playerCamera;
+    public float distance = 5f;
+    public LayerMask itemLayer;
+    private Color white = Color.white;
+    private Color red = Color.red;
+    public Material mat;
+
+    void Start() {
+        mat.SetFloat("_TriggerValue", 0f);
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        if (PlayerTrigger.playerInside) {
+            if (Physics.Raycast(ray, distance, itemLayer))
+            {
+                crosshair.color = red;
+                mat.SetFloat("_TriggerValue", 1f);
+            }
+            else
+            {
+                crosshair.color = white;
+                mat.SetFloat("_TriggerValue", 0f);
+
+            }
+        }
+        else {
+            crosshair.color = white;
+            mat.SetFloat("_TriggerValue", 0f);
+        }
+
+    }
+}
---
+++ ChangeMaterial/Aim - Crosshair/PlayerTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+    public static bool playerInside;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInside = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInside = false;
+    }
+}
